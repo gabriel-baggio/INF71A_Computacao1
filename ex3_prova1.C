637e33713b063abe3257aafed6278d31a1583bba
gabriel baggio <114102350+gabriel-baggio@users.noreply.github.com>
2025-04-23 10:14:33 -0300

    Update and rename ex1_prova3.C to ex3_prova1.C

--- ex3_prova1.C
+++ ex3_prova1.C
@@ -3,29 +3,29 @@
 
 int main()
 {
-    int força, N_arma;
+    int forÃ§a, N_arma;
     float critico, dano;
 
-    printf("digite a força do personagem (entre 1-100): ");
-    scanf("%d", &força);
-    while(força < 1 || força > 100){
-        printf("valor de força invalido. \nDigite um valor valido: ");
-        scanf("%d", &força);
+    printf("digite a forÃ§a do personagem (entre 1-100): ");
+    scanf("%d", &forÃ§a);
+    while(forÃ§a < 1 || forÃ§a > 100){
+        printf("valor de forÃ§a invalido. \nDigite um valor valido: ");
+        scanf("%d", &forÃ§a);
     }
     printf("digite o nivel da arma (entre 1-50): ");
     scanf("%d", &N_arma);
     while(N_arma < 1 || N_arma > 50){
-        printf("valor do nivel de arma é invalido. \nDigite um valor valido: ");
+        printf("valor do nivel de arma Ã© invalido. \nDigite um valor valido: ");
         scanf("%d", &N_arma);
     }
-    printf("digite a multiplacação do critico (entre 1.0-2.0): ");
+    printf("digite a multiplacaÃ§Ã£o do critico (entre 1.0-2.0): ");
     scanf("%f", &critico);
     while(critico < 1 || critico > 2){
-        printf("valor da multiplacação do critico é invalido. \nDigite um valor valido: ");
+        printf("valor da multiplacaÃ§Ã£o do critico Ã© invalido. \nDigite um valor valido: ");
         scanf("%f", &critico);
     }
 
-    dano = força * N_arma * critico;
+    dano = forÃ§a * N_arma * critico;
 
     if(dano >= 5000){
         printf("DANO CRITICO MASSIVO!");
